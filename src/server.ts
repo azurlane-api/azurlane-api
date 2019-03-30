8af0d56358c4baf80acb2963616e6883ccb5a3da
KurozeroPB <root@kurozeropb.info>
2019-03-30 16:28:42 +0100

    Add landing page

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import settings from "../settings";
 import APIv1 from "./api/v1/Router";
 
@@ -9,11 +10,20 @@ const api = new APIv1(settings);
 async function main() {
     await api.loadRoutes();
 
-    app.set("env", settings.env);
+    app.enable("trust proxy");
     app.disable("x-powered-by");
+
+    app.set("views", path.join(__dirname, "views"));
+    app.set("view engine", 'ejs');
+    app.set("json spaces", 4);
+    app.set("env", settings.env);
 
     app.use(express.json());
     app.use(api.path, api.router);
+
+    app.get("/", (_req, res) => {
+        res.render("index")
+    });
 
     app.listen(port, (error: Error) => {
         if (error) return console.error(error);
