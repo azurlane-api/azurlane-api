8b5b379383ced8684773d1749d2e1798e4e711cf
KurozeroPB <root@kurozeropb.info>
2019-06-22 14:37:04 +0200

    Update deps and copyAssets script

--- src/tools/copyAssets.ts
+++ src/tools/copyAssets.ts
@@ -1,4 +1,12 @@
-import * as shell from "shelljs";
+import { exec } from "child_process";
 
 // Copy all the view templates
-shell.cp( "-R", "src/views", "build/src" );
+exec("cp -R src/views build/src", (error, _stdout, stderr) => {
+    if (error) {
+        console.error(error);
+        process.exit(1);
+    } else if (stderr) {
+        console.error(stderr);
+        process.exit(1);
+    }
+});
